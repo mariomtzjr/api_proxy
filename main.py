0f77fba360d71a42b53d191b86ccb605cfedcc6a
Mario Martinez <francisco.smtz@gmail.com>
2024-03-31 01:35:33 -0600

    feat: Add first approach to 'proxy' with poke api

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+import os
+
+import httpx
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"details": "Hello, World!"}
+app = FastAPI()
+
+@app.get("/{path:path}")
+async def proxy(path: str, request: Request):
+    target_url = f"{os.environ.get('URL_POKE_API', "")}{path}"
+    async with httpx.AsyncClient() as client:
+        response = await client.request(
+            method=request.method,
+            url=target_url,
+            timeout=None
+        )
+        return response.json()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, host="0.0.0.0", log_level="info")
